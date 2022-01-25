a54beae07d7ac63986263771b6f842f2d4422923
dexat0r <nikit-balak@yandex.ru>
2022-01-25 16:46:38 +0300

    refactor

--- src/services/bot/index.ts
+++ src/services/bot/index.ts
@@ -44,6 +44,7 @@ export default class BotService {
             };
         } else {
             this.botUsers[user].stage = 0;
+            console.log(this.botUsers[user].stage)
         }
         
         await ctx.reply("Привет!\nЯ помогу тебе подготовиться к предстоящему собеседованию!\nПриступим?");
